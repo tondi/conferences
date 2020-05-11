909fcb744d6b0a98063a3e44d764443c309bcd17
Hodzinets <godzinets35@gmail.com>
2020-05-11 18:41:02 +0200

    Updated creation instatnces

--- AddInstancesProcedures.sql
+++ AddInstancesProcedures.sql
@@ -1,5 +1,52 @@
-CREATE PROCEDURE Add_Conference(@Name varchar(255), @StartDate date, @EndDate date,  @StudentDiscount int)
+CREATE OR ALTER PROCEDURE Add_Conference(@Name varchar(255), @StartDate date, @EndDate date,  @StudentDiscount int)
 AS
 BEGIN
 INSERT INTO Conference (Name, StartDate, EndDate, StudentDiscount) VALUES(@Name, @StartDate, @EndDate, @StudentDiscount);
 END;
+GO
+
+CREATE OR ALTER  PROCEDURE Add_ConferenceDay(@ConferenceId int, @Date date ,@Capacity int, @LocationId int ,@Price decimal )
+AS
+BEGIN
+
+DECLARE @count int;
+SET @count = (SELECT COUNT(1) from ConferenceDay
+where ConferenceDay.Date = @Date AND ConferenceDay.ConferenceId = @ConferenceId AND ConferenceDay.LocationId = @LocationId)
+
+IF @count = 0 
+INSERT INTO ConferenceDay (ConferenceId, Date, Capacity, LocationId, Price) VALUES(@ConferenceId, @Date, @Capacity, @LocationId, @Price);
+ELSE RAISERROR('This conference day already exists!', 15, 15); 
+
+END;
+GO
+
+CREATE OR ALTER PROCEDURE Add_Workshop(@Name varchar(255), @ConferenceDayId int , @Capacity int , @StartDateTime datetime , @EndDateTime datetime , @DurationMinutes int , @Price decimal , @AddressId int )
+AS
+BEGIN
+INSERT INTO Workshop (Name, ConferenceDayId, Capacity, StartDateTime, DurationMinutes, Price, AddressId) VALUES(@Name, @ConferenceDayId, @Capacity, @StartDateTime, @DurationMinutes, @Price, @AddressId);
+END;
+GO
+
+CREATE OR ALTER PROCEDURE Add_Location(@City varchar(255), @Country varchar(255))
+AS
+BEGIN
+
+DECLARE @count int;
+SET @count = (SELECT COUNT(1) from Location
+where Location.City = @City AND Location.Country = @Country);
+
+if @count = 0
+INSERT INTO Location(City, Country) VALUES(@City, @Country);
+ELSE RAISERROR('This location already exists!', 15, 15); 
+
+END;
+GO
+
+CREATE OR ALTER PROCEDURE Add_Address(@LocationId int, @Street varchar(255), @BuildingNumber varchar(255),  @Room varchar(255))
+AS
+BEGIN
+INSERT INTO Address(LocationId, Street, BuildingNumber, Room) VALUES(@LocationId, @Street, @BuildingNumber, @Room);
+END;
+GO
+
+
