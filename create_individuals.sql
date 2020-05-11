194a6797b1fcc8aa4c57afc70eea8b21e5cb8640
Hodzinets <godzinets35@gmail.com>
2020-05-11 18:57:12 +0200

    Added reservation on conference day

--- create_individuals.sql
+++ create_individuals.sql
@@ -11,3 +11,17 @@ BEGIN
 INSERT INTO dbo.Person (Name, CompanyId, StudentIdNumber) VALUES(@Name, @CompanyId, @StudentIdNumber);
 END;
 go
+
+CREATE OR ALTER PROCEDURE Add_CompanyConferenceDayReservation(@ConferenceDayId int, @CompanyId int, @NumberOfPersons int)
+AS
+BEGIN
+INSERT INTO CompanyConferenceDayReservation(ConferenceDayId, NumberOfPersons, CompanyId, Paid, ReservationDate) VALUES(@ConferenceDayId, @NumberOfPersons, @CompanyId, 0, GETDATE());
+END;
+GO
+
+CREATE OR ALTER PROCEDURE Add_PersonConferenceDayReservation(@ConferenceDayId int, @PersonId int, @CompanyReservationId int)
+AS
+BEGIN
+INSERT INTO PersonConferenceDayReservation(ConferenceDayId, PersonId, CompanyReservationId, Paid, ReservationDate) VALUES(@ConferenceDayId, @PersonId, @ConferenceDayId, 0, GETDATE());
+END;
+GO
