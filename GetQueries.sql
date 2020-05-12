e47b3e5424df94f1a07f5d5d1dbd1e2415e1592f
Hodzinets <godzinets35@gmail.com>
2020-05-12 19:03:48 +0200

    Updated all Get Functions

--- GetQueries.sql
+++ GetQueries.sql
@@ -20,5 +20,67 @@ AND (SELECT COUNT(*) FROM PersonConferenceDayReservation INNER JOIN CompanyConfe
 ORDER BY ConferenceDay.Date;
 
 RETURN
-END;  
+END  
+GO
+
+CREATE OR ALTER FUNCTION Get_Capacity_Left_For_ConferenceDay(@ConferenceDayId int)  
+RETURNS int   
+AS  
+BEGIN  
+    DECLARE @ret int;  
+
+	SET @ret = (SELECT TOP 1 Capacity FROM ConferenceDay WHERE ConferenceDay.Id = @ConferenceDayId) 
+	- (SELECT SUM(CompanyConferenceDayReservation.NumberOfPersons) FROM CompanyConferenceDayReservation)
+	- (SELECT COUNT(*) FROM PersonConferenceDayReservation WHERE PersonConferenceDayReservation.CompanyReservationId = NULL);
+
+    RETURN @ret;  
+END 
+GO
+
+CREATE OR ALTER FUNCTION Get_Capacity_Left_For_Workshop(@WorkshopId int)  
+RETURNS int   
+AS  
+BEGIN  
+    DECLARE @ret int;  
+
+	SET @ret = (SELECT TOP 1 Capacity FROM Workshop WHERE Workshop.Id = @WorkshopId) 
+	- (SELECT SUM(CompanyWorkshopReservation.NumberOfPersons) FROM CompanyWorkshopReservation)
+	- (SELECT COUNT(*) FROM PersonWorkshopReservation WHERE PersonWorkshopReservation.CompanyReservationId = NULL);
+
+    RETURN @ret;  
+END 
+GO
+
+CREATE OR ALTER FUNCTION Get_ConferenceDay_Price(@ConferenceDayId int, @IsStuden bit, @RequestedDate date)  
+RETURNS int   
+AS  
+BEGIN  
+    DECLARE @conferenceId int;
+	SET @conferenceId = (SELECT TOP 1 ConferenceId FROM ConferenceDay WHERE ConferenceDay.Id = @ConferenceDayId) 
+	
+    DECLARE @conferenceStartDate date;
+	SET @conferenceStartDate = (SELECT TOP 1 StartDate FROM Conference WHERE Conference.Id = @conferenceId) 
+
+    DECLARE @price int;
+	SET @price = (SELECT TOP 1 Price FROM ConferenceDay WHERE ConferenceDay.Id = @ConferenceDayId) 
+
+    DECLARE @studentDiscount int;
+	SET @studentDiscount = (SELECT TOP 1 StudentDiscount FROM Conference WHERE Conference.Id = @conferenceId) 
+
+	DECLARE @discount int;
+	SET @discount = (SELECT TOP 1 Percentage FROM Discount WHERE Discount.ConferenceId = @conferenceId AND Discount.DaysToConference >= DATEDIFF(DAY,@RequestedDate, @conferenceStartDate)
+															ORDER BY DaysToConference)
+
+
+	DECLARE @result int;
+	SET @result = @price
+
+	if @IsStuden > 0
+	SET @result -= @price * @studentDiscount /100
+
+	IF @discount IS NOT NULL
+	SET @result -= @price * @discount /100
+
+    RETURN @result;  
+END 
 GO
